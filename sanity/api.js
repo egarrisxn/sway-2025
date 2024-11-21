ec514ff58aa7d86e2833350ace8c0f62aa288c8d
egarrisxn <egarrisxn@gmail.com>
2024-11-20 23:13:14 -0500

    yeah!

--- sanity/api.js
+++ sanity/api.js
@@ -6,7 +6,6 @@ export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-11
 
 export const studioUrl = process.env.NEXT_PUBLIC_SANITY_STUDIO_URL || 'http://localhost:3333'
 
-export const previewUrl =
-  process.env.NEXT_PUBLIC_SANITY_STUDIO_PREVIEW_URL || 'http://localhost:3000'
+export const previewUrl = process.env.NEXT_PUBLIC_SANITY_PREVIEW_URL || 'http://localhost:3000'
 
 //
